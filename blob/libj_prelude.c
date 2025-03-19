698697ba9c0abaa1b6ec02ecc2d4595a6ca29d77
Julie B. <no-reply@bbjubjub.fr>
2025-03-19 14:52:43 +0100

    format

--- blob/libj_prelude.c
+++ blob/libj_prelude.c
@@ -31,56 +31,55 @@ void *memzero(char *buf, unsigned n) {
 }
 
 int syscall3(int ecall_type, void *dst, int dst_len_in_words, int, int, int) {
-		asm(
-	    "rd_t0 rs1_sp !20 lw"
-	    "rd_a0 rs1_sp !16 lw"
-	    "rd_a1 rs1_sp !12 lw"
-	    "rd_a2 rs1_sp !8 lw"
-	    "rd_a3 rs1_sp !4 lw"
-	    "rd_a4 rs1_sp !0 lw"
-    "ecall"                 // ecall(t0, a0, a1, a2, a3, a4)
-	    );
+	asm(
+			"rd_t0 rs1_sp !20 lw"
+			"rd_a0 rs1_sp !16 lw"
+			"rd_a1 rs1_sp !12 lw"
+			"rd_a2 rs1_sp !8 lw"
+			"rd_a3 rs1_sp !4 lw"
+			"rd_a4 rs1_sp !0 lw"
+			"ecall"                 // ecall(t0, a0, a1, a2, a3, a4)
+	   );
 }
 
 int syscall4(int ecall_type, void *dst, int dst_len_in_words, int, int, int, int) {
-		asm(
-	    "rd_t0 rs1_sp !24 lw"
-	    "rd_a0 rs1_sp !20 lw"
-	    "rd_a1 rs1_sp !16 lw"
-	    "rd_a2 rs1_sp !12 lw"
-	    "rd_a3 rs1_sp !8 lw"
-	    "rd_a4 rs1_sp !4 lw"
-	    "rd_a5 rs1_sp !0 lw"
-    "ecall"                 // ecall(t0, a0, a1, a2, a3, a4, a5)
-	    );
+	asm(
+			"rd_t0 rs1_sp !24 lw"
+			"rd_a0 rs1_sp !20 lw"
+			"rd_a1 rs1_sp !16 lw"
+			"rd_a2 rs1_sp !12 lw"
+			"rd_a3 rs1_sp !8 lw"
+			"rd_a4 rs1_sp !4 lw"
+			"rd_a5 rs1_sp !0 lw"
+			"ecall"                 // ecall(t0, a0, a1, a2, a3, a4, a5)
+	   );
 }
 
 int ecall1(int, int, int) {
-		asm(
-	    "rd_t0 rs1_sp !8 lw"
-	    "rd_a0 rs1_sp !4 lw"
-	    "rd_a1 rs1_sp !0 lw"
-	    "ecall"                 // ecall(t0, a0, a1)
-	    );
+	asm(
+			"rd_t0 rs1_sp !8 lw"
+			"rd_a0 rs1_sp !4 lw"
+			"rd_a1 rs1_sp !0 lw"
+			"ecall"                 // ecall(t0, a0, a1)
+	   );
 }
 
 int write(int fd, char *data, unsigned nbytes) {
 	return syscall4(ECALL_SOFTWARE, 0, 0, SYS_WRITE, fd, data, nbytes);
 }
 int read(int fd, char *buf, unsigned nbytes) {
-	// TODO handle weird last word behaviour
-    return syscall4(ECALL_SOFTWARE, buf, (nbytes + 3)/4, SYS_READ, fd, buf, nbytes);
+	return syscall4(ECALL_SOFTWARE, buf, (nbytes + 3)/4, SYS_READ, fd, buf, nbytes);
 }
 
 void sha_compress(unsigned *sha_state, unsigned *blocks, unsigned block_count) {
-    syscall3(
-        ECALL_SHA,
-        sha_state,
-        sha_state,
-        blocks,
-        blocks + DIGEST_BYTES,
-        block_count
-    );
+	syscall3(
+			ECALL_SHA,
+			sha_state,
+			sha_state,
+			blocks,
+			blocks + DIGEST_BYTES,
+			block_count
+		);
 }
 
 unsigned htonl(unsigned x) {
@@ -118,13 +117,13 @@ void sha_finalize(unsigned *sha_state, char *last_block, unsigned total_bytes) {
 }
 
 void halt(unsigned user_exit, unsigned *out_state) {
-    ecall1(
-        SYS_HALT,
-        HALT_TERMINATE | (user_exit << 8),
-        out_state
-    );
+	ecall1(
+			SYS_HALT,
+			HALT_TERMINATE | (user_exit << 8),
+			out_state
+	      );
 
-    // unreachable
+	// unreachable
 }
 
 
@@ -169,7 +168,7 @@ int getchar() {
 		// EOF
 		return -1;
 	}
-		char c = load_char_unaligned(buf_stdin + (stdin_offset % 64));
+	char c = load_char_unaligned(buf_stdin + (stdin_offset % 64));
 	stdin_offset += 1;
 	return c;
 }
