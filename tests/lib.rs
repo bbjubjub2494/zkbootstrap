bf1d1805e2b98bcaee250986e79dd705b9c40e09
Julie B. <no-reply@bbjubjub.fr>
2025-03-29 23:55:04 +0100

    wip

--- tests/lib.rs
+++ tests/lib.rs
@@ -10,6 +10,10 @@ fn jcat_program() -> Vec<u8> {
     Blobs::get("derived/jcat").unwrap().data.to_vec()
 }
 
+fn jhex0_program() -> Vec<u8> {
+    Blobs::get("derived/jhex").unwrap().data.to_vec()
+}
+
 #[test]
 pub fn test_jcat() -> Result<()> {
     let output = execute(&jcat_program(), b"hello", None::<&mut std::io::Stderr>)?;
@@ -20,9 +24,27 @@ pub fn test_jcat() -> Result<()> {
 #[test]
 pub fn test_jcat_reference() -> Result<()> {
     let program = methods::JCAT_ELF;
-    // NOTE: bug in the reference code: it pads the input with zeroes up to a multiple of 4 bytes
+    // NOTE: the reference code pads the input with zeroes up to word boundary
+    // Is this a bug in this version of the risc0 stdlib?
     let input_bytes = b"hello\0\0\0";
     let output_bytes = execute(program, input_bytes, None::<&mut std::io::Stderr>)?;
     assert_eq!(output_bytes, input_bytes);
+    Ok(())
+}
+
+#[test]
+pub fn test_jhex0() -> Result<()> {
+    let input_bytes = b"7465 7374 0a";
+    let output_bytes = execute(&jhex0_program(), input_bytes, None::<&mut std::io::Stderr>)?;
+    assert_eq!(output_bytes, b"test\n");
+    Ok(())
+}
+
+#[test]
+pub fn test_jhex0_reference() -> Result<()> {
+    let program = methods::JHEX0_ELF;
+    let input_bytes = b"7465 7374 0a";
+    let output_bytes = execute(program, input_bytes, None::<&mut std::io::Stderr>)?;
+    assert_eq!(output_bytes, b"test\n");
     Ok(())
 }
